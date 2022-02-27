c72f8ff44e9164e2243092cfd3601079fb22c74a
Justin <vien_justin@hotmail.com>
2022-02-27 11:46:12 -0500

    added initial search functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import { Link, useNavigate } from "react-router-dom"
 import { IoIosSearch, IoMdClose } from "react-icons/io"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const NavBar = () => {
   const [isInputShown, setIsInputShown] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
   let navigate = useNavigate();
 
   const handleSearchClick = () => {
@@ -17,8 +19,20 @@ const NavBar = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault();
     console.log('submitted')
-    navigate('/results')
+    navigate('/results', {state: searchResults})
   }
+
+  useEffect(()=>{
+    axios({
+      url: 'https://api.themoviedb.org/3/search/movie?query=spiderman',
+      params: {
+        api_key: 'abca8adda9e521b362fff5ab08ec8402', 
+      }
+    }).then((res) => {
+      console.log(res)
+      setSearchResults(res.data.results)
+    })
+  }, [])
 
   return (
     <nav>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,21 @@
+import { useLocation } from "react-router-dom"
+import Movies from "./Movies";
+
 const SearchResults = () => {
+  const { state } = useLocation();
+  console.log(state)
+
+
   return (
-    <div>SearchResults</div>
+    <section className="searchPage">
+      {state.map(movie => {
+        return (
+          <div>
+            <div className="movieTitle">{movie.title}</div>
+          </div>
+        )
+      })}
+    </section>
   )
 }
 
